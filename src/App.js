6ccd4aec0a5380c51097933f8a9c5f719febb71d
sachin <sangwansachin631@gmail.com>
2023-03-09 10:58:35 +0530

    enter for check

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 
 import './App.css';
+import Navbar from './components/navbar/Navbar';
 
 function App() {
   return (
     <div className="App">
-      <h1 className='text-red-500'>Sachin Sangwan</h1>
+      <h1 className='text-red-500'>Manoj Sangwan Sangwan</h1>
+      <Navbar/>
     </div>
   );
 }
